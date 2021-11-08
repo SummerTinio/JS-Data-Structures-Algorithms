e6b3b9b534342fbe1e412d6f849198e3b44dadb7
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-11-08 18:24:20 +0800

    SLL: Add, test push & unshift (insertion on ends)

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -11,6 +11,28 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
+  push(val) {
+    if (this.isEmpty()) {
+      this.head = this.tail = new SLLNode(val);
+    } else {
+      this.tail = this.tail.next = new SLLNode(val);
+    }
+    this.length++;
+    return this;
+  }
+
+  unshift(val) {
+    if (this.isEmpty()) {
+      this.head = this.tail = new SLLNode(val);
+    } else {
+      let newFirstNode = new SLLNode(val);
+      newFirstNode.next = this.head;
+      this.head = newFirstNode;
+    }
+    this.length++;
+    return this;
+  }
+
   isEmpty() {
     return this.head === null;
   }
