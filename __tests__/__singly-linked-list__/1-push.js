a4ecb362a13555253d9bbe9e854c2f142e4e0eec
Summer Tinio <79723484+SummerTinio@users.noreply.github.com>
2021-11-13 18:57:45 +0800

    refactor SLL: push, unshift descriptions, structure

--- __tests__/__singly-linked-list__/1-push.js
+++ __tests__/__singly-linked-list__/1-push.js
@@ -6,33 +6,36 @@ beforeEach(() => {
   list = new SinglyLinkedList();
 });
 
-describe('push method', () => {
-  it('handles edge case: <1 node SLL (Empty List)', () => {
-    list.push('lone item');
-    expect(list.head.val).toBe('lone item');
-    expect(list.tail.val).toBe('lone item');
-    expect(list.length).toBe(1);
-  });
-  
-  it('handles edge case: 1 node SLL', () => {
-    list.push('item 1 of 2');
-    list.push('item 2 of 2');
-    expect(list.head.val).toBe('item 1 of 2');
-    expect(list.tail.val).toBe('item 2 of 2');
-    expect(list.head.next.val).toBe('item 2 of 2');
-    expect(list.length).toBe(2);
-  });
-  
-  it('handles typical case: >1 node SLL', () => {
-    list.push('A');
-    list.push('B');
-    list.push('C');
-    // test labels (i.e. 'head,' 'tail)
-    expect(list.head.val).toBe('A');
-    expect(list.tail.val).toBe('C');
-    // test the links (i.e. pointers or .next)
-    expect(list.head.next.val).toBe('B');
-    // test the length
-    expect(list.length).toBe(3);
-  });
+describe('push() method', () => {
+  describe('handles 2 List Appending Cases ( <1 node, >=1 node )', () => {
+    it('handles edge case: <1 node SLL (Empty List)', () => {
+      list.push('lone item');
+      expect(list.head.val).toBe('lone item');
+      expect(list.tail.val).toBe('lone item');
+      expect(list.length).toBe(1);
+    });
+
+    describe('handles typical case: >=1 node SLL', () => {
+      it('handles typical case: 1 node SLL', () => {
+        list.push('item 1 of 2');
+        list.push('item 2 of 2');
+        expect(list.head.val).toBe('item 1 of 2');
+        expect(list.tail.val).toBe('item 2 of 2');
+        expect(list.head.next.val).toBe('item 2 of 2');
+        expect(list.length).toBe(2);
+      });
+      it('handles typical case: >1 node SLL', () => {
+        list.push('A');
+        list.push('B');
+        list.push('C');
+        // test labels (i.e. 'head,' 'tail)
+        expect(list.head.val).toBe('A');
+        expect(list.tail.val).toBe('C');
+        // test the links (i.e. pointers or .next)
+        expect(list.head.next.val).toBe('B');
+        // test the length
+        expect(list.length).toBe(3);
+      });
+    });
+  }); 
 });
--- __tests__/__singly-linked-list__/1-unshift.js
+++ __tests__/__singly-linked-list__/1-unshift.js
@@ -6,37 +6,41 @@ beforeEach(() => {
   list = new SinglyLinkedList();
 });
 
-describe('unshift method', () => {
-  it('handles edge case: <1 node (Empty List)', () => {
-    list.unshift('lone node');
-    // list itself
-    expect(list.head.val).toBe('lone node');
-    expect(list.length).toBe(1);
-    // lone node
-      // links
-    expect(list.head.next).toBe(null);
-  });
+describe('unshift() method', () => {
+  describe('handles 2 List Appending Cases ( <1 node, >=1 node )', () => {
+    it('handles edge case: <1 node (Empty List)', () => {
+      list.unshift('lone node');
+      // list itself
+      expect(list.head.val).toBe('lone node');
+      expect(list.length).toBe(1);
+      // lone node
+        // links
+      expect(list.head.next).toBe(null);
+    });
 
-  it('handles edge case: 1 node SLL', () => {
-    list.unshift('B');
-    list.unshift('A');
-    // list itself
-      // length
-      expect(list.length).toBe(2);
-      // labels
-      expect(list.head.val).toBe('A');
-      expect(list.tail.val).toBe('B');
-  });
-  
-  it('handles typical case: >1 node SLL', () => {
-    list.unshift('C');
-    list.unshift('B');
-    list.unshift('A');
-    // list itself
-      // length
-    expect(list.length).toBe(3);
-      // labels
-    expect(list.head.val).toBe('A');
-    expect(list.tail.val).toBe('C');
+    describe('handles typical case: >=1 node', () => {
+      it('handles typical case: 1 node SLL', () => {
+        list.unshift('B');
+        list.unshift('A');
+        // list itself
+          // length
+          expect(list.length).toBe(2);
+          // labels
+          expect(list.head.val).toBe('A');
+          expect(list.tail.val).toBe('B');
+      });
+      
+      it('handles typical case: >1 node SLL', () => {
+        list.unshift('C');
+        list.unshift('B');
+        list.unshift('A');
+        // list itself
+          // length
+        expect(list.length).toBe(3);
+          // labels
+        expect(list.head.val).toBe('A');
+        expect(list.tail.val).toBe('C');
+      });
+    });
   });
 });
